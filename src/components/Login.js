ed688e337138a3132e7c1980c561f40b808a6dbc
Mani-Ofcl <karupu@whydigit.in>
2025-03-22 18:31:10 +0530

    upd

--- src/components/Login.js
+++ src/components/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
         {error && <p className="text-danger text-center">{error}</p>}
 
         <div className="mb-3">
-          <label className="form-label">Email / UserNa</label>
+          <label className="form-label">Email / UserName</label>
           <input
             type="email"
             className="form-control"
